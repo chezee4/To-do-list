a0045222a60c6ea524d42f350f40b8545d7c8753
chezee4 <vadimdufinec@gmail.com>
2023-06-26 01:01:59 +0300

    refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,105 +1,89 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".to-do__form");
+  const input = document.querySelector(".input");
+  const toDoList = document.querySelector(".list");
+  const errorMessage = document.querySelector(".text");
 
+  const createListItem = (task, id, isActive = false) => {
+    const toDoItem = document.createElement("li");
+    toDoItem.classList.add("list__item");
+    if (isActive) {
+      toDoItem.classList.add("active");
+    }
+    toDoItem.textContent = task;
+    toDoItem.dataset.id = id;
+    const btnTrash = document.createElement("button");
+    btnTrash.classList.add("delete");
+    toDoItem.append(btnTrash);
+    toDoList.prepend(toDoItem);
+  };
+  const getData = () => {
+    request().then((data) =>
+      data.forEach((item) => createListItem(item.title, item.id))
+    );
+  };
 
- window.addEventListener("DOMContentLoaded", () =>{
+  const request = async (
+    url = "https://jsonplaceholder.typicode.com/posts?_start=0&_limit=4",
+    method = "GET",
+    body = null,
+    headers = { "Content-Type": "application/json" }
+  ) => {
+    try {
+      const response = await fetch(url, { method, headers, body });
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const form = document.querySelector('.to-do__form');
-    const input = document.querySelector('.input');
-    const list = document.querySelector('.list');
-    const  errorMessage  = document.querySelector('.text');
-  
+  toDoList.addEventListener("click", (event) => {
+    if (event.target.tagName === "LI") {
+      const toDoItem = event.target;
+      const id = toDoItem.dataset.id;
+      const isActive = !toDoItem.classList.contains("active");
+      request(
+        `https://jsonplaceholder.typicode.com/posts/${id}`,
+        "PATCH",
+        JSON.stringify({ isActive })
+      );
+      toDoItem.classList.toggle("active");
+    }
+  });
 
-        const createListItem = (task, id, isActive = false) =>{
-            const li = document.createElement('li');
-            li.classList.add('list__item');
-            if (isActive) {
-                li.classList.add('active');
-            }
-            li.textContent = task;
-            li.dataset.id = id;
-            const img = document.createElement('img');
-            img.classList.add("delete");
-            img.src = "./assets/icon/trash.png";
-            li.append(img);
-            list.prepend(li);
-        }
-            const  getData = async() => {
-            try {
-                const response = await fetch('http://localhost:3000/list');
-                const data = await response.json();
-                await  data.forEach(item => {
-                       createListItem(item.task, item.id, item.isActive);
-                });
-            } catch (reject) {
-                console.log("Упс...");
-            }
-        }
+  const checkedValue = (event) =>
+    event.target.value.length >= 35
+      ? (errorMessage.style.display = "block")
+      : (errorMessage.style.display = "none");
+  input.oninput = (event) => checkedValue(event);
+  form.onsubmit = (event) => {
+    event.preventDefault();
+    const task = input.value;
+    if (task.trim() === "") {
+      form.reset();
+      errorMessage.style.display = "none";
+      return;
+    }
+    form.reset();
+    request(
+      "https://jsonplaceholder.typicode.com/posts/",
+      "POST",
+      JSON.stringify({ title: task })
+    ).then((e) => createListItem(e.title, e.id));
+  };
 
-        list.addEventListener('click', async event => {
-            if (event.target.tagName === "LI") {
-                const li = event.target;
-                const id = li.dataset.id;
-                const isActive = !li.classList.contains('active');
-                try {
-                    await fetch(`http://localhost:3000/list/${id}`, {
-                        method: 'PATCH',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify( {isActive} )
-                    });
-                    li.classList.toggle('active');
-                } catch (reject) {
-                    console.log("Упс...");
-                }
-            }
-        });
-         
-        const checkedValue = event => event.target.value.length >= 35? errorMessage.style.display = "block": errorMessage.style.display = "none"
-        input.addEventListener('input', checkedValue);
+  toDoList.onclick = (event) => {
+    if (event.target.tagName === "BUTTON") {
+      const toDoItem = event.target.parentNode;
+      const id = toDoItem.dataset.id;
+      request(`https://jsonplaceholder.typicode.com/posts/${id}`, "DELETE");
+      toDoList.removeChild(toDoItem);
+    }
+  };
 
-
-        form.onSubmit = async event => {
-            event.preventDefault();
-            const task = input.value;
-            if (task.trim() === ""|| task.length>=35) {
-                form.reset(); 
-                errorMessagep.style.display ="none";
-                return;
-            }
-            
-            form.reset(); 
-            try {
-                const response = await fetch('http://localhost:3000/list', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ task })
-                });
-                const data = await response.json();
-                createListItem(task, data.id);
-            } catch (reject) {
-                console.log("Упс...");
-            }
-        });
-        
-
-
-        list.addEventListener('click', async event => {
-            if (event.target.tagName === "IMG") {
-                const li = event.target.parentNode;
-                const id = li.dataset.id;
-                try {
-                     await fetch(`http://localhost:3000/list/${id}`, {
-                        method: 'DELETE'
-                    });
-                    list.removeChild(li);
-                } catch (reject) {
-                    console.log("Упс...");
-                }
-            }
-        });
-        
-        getData();
-
- });
+  getData();
+});
